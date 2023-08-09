864b55e82f63f6e9cae3927701f686b026df2dd8
Mateo Perez Rivera <matperez@itba.edu.ar>
2023-08-09 18:49:22 -0300

    graphs working

--- TP1/src/main/python/graph.py
+++ TP1/src/main/python/graph.py
@@ -22,7 +22,7 @@ def get_particles_data(file_name):
             x = particle["x"]
             y = particle["y"]
             r = particle["r"]
-            if maxR <= r:
+            if maxR >= r:
                 maxR = r
             particleArray.append(Particle(particle_id, x, y, r))
         M = int(math.floor(L / (Rc + 2 * maxR)))
@@ -34,8 +34,12 @@ def getParticle(particle, particles):
             return p
 
 def main():
-    id_particle = 'p1'
+    id_particle = 'p2'
     particles, L, N, M = get_particles_data("static.json")
+    print(particles)
+    print(L)
+    print(N)
+    print(M)
     time = 0
     with open('output.txt') as particles_data:
         next(particles_data)  # Skip first line
@@ -57,23 +61,37 @@ def main():
     for i in range(M + 1):
         ax.axhline(i * L, color='gray', linewidth=0.5)
         ax.axvline(i * L, color='gray', linewidth=0.5)
-
-    # Plot particles as dots
-    for particle in particles:
-        circle = plt.Circle((particle.x, particle.y), particle.r, color='blue', fill=False)
-        ax.add_patch(circle)
-
-    # Highlight neighbors
     index = 0
     for particle in particles:
         if particle.id == id_particle:
+            print("found")
             break
         else:
             index += 1
+    # Plot particles as dots
+    for i in range(len(particles)):
+        if (index==i):
+            circle = plt.Circle((particles[i].x, particles[i].y), particles[i].r, color='black', fill=True) 
+        else:
+            circle = plt.Circle((particles[i].x, particles[i].y), particles[i].r, color='blue', fill=True) 
+        ax.add_patch(circle)
+
+    # Highlight neighbors
     for particle in particles[index].neighbours:
         p = getParticle(particle, particles)
-        plt.Circle((p.x, p.y), p.r, color='red', fill=True)
+        print(p.r)
+        circle =plt.Circle((p.x, p.y), p.r, color='red', fill=True)
+        ax.add_patch(circle)
+    # Customize the plot
+    plt.xlim(0, M * L)
+    plt.ylim(0, M * L)
+    plt.title("Particles and Neighbors")
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.gca().set_aspect('equal', adjustable='box')
 
+    # Show the plot
+    plt.show()
 
 if __name__ == "__main__":
     main()
